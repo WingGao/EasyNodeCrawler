52816a848ee8add9251ec7b100c1fc7f7ae868cf
Wing Gao <wing.gao@live.com>
2017-08-05 01:32:58 +0800

    添加错误处理

--- crawlers/zxcs8/index.js
+++ crawlers/zxcs8/index.js
@@ -13,13 +13,13 @@ function fetchRank(pid) {
         request(`http://www.zxcs8.com/content/plugins/cgz_xinqing/cgz_xinqing_action.php?action=show&id=${pid}`, function (error, response, body) {
             if (error != null) {
                 logger.error('[fetchRank-error]', pid, error)
-                reject()
+                reject(pid)
                 return
             }
             let statusCode = _.get(response, 'statusCode')
             if (statusCode !== 200) {
                 logger.error('[fetchRank-error]', pid, `code=${statusCode}`)
-                reject()
+                reject(pid)
                 return
             }
 
@@ -39,13 +39,13 @@ function fetchPost(pid) {
         request(`http://www.zxcs8.com/post/${pid}`, function (error, response, body) {
             if (error != null) {
                 logger.error('[fetchPost-error]', pid, error)
-                reject()
+                reject(pid)
                 return
             }
             let statusCode = _.get(response, 'statusCode')
             if (statusCode !== 200) {
                 logger.error('[fetchPost-error]', pid, `code=${statusCode}`)
-                reject()
+                reject(pid)
                 return
             }
 
@@ -53,10 +53,17 @@ function fetchPost(pid) {
                 const $ = cheerio.load(body)
                 const content = $('#content')
                 let post_title = content.find('h1').text()
+                let bookname = _.get(post_title.match(/《(.*?)》/), '1')
+                if (bookname == null) {
+                    logger.error('[fetchPost-error]', pid, 'no bookname', post_title)
+                    reject(pid)
+                    return
+                }
+
                 let post = {
                     pid: pid,
                     post_title: post_title,
-                    book_name: post_title.match(/《(.*?)》/)[1],
+                    book_name: bookname,
                     category: content.find('p.date a').slice(1).eq(0).text(),
                 }
 
@@ -73,15 +80,70 @@ function fetchPost(pid) {
                 } else {
                     _fetch()
                 }
+            }).catch(err => {
+                logger.error('[fetchPost-error]', err)
+                reject(pid)
             })
         });
     })
 }
 
+function getMaxPid() {
+    return new Promise((resolve, reject) => {
+        request(`http://www.zxcs8.com/map.html`, function (error, response, body) {
+            if (error != null) {
+                logger.error('[getMaxPid-error]', error)
+                reject()
+                return
+            }
+
+            const $ = cheerio.load(body)
+            let maxPid = $('#content a').attr('href').match(/\/(\d+)/)[1]
+            logger.info('max pid', maxPid)
+            resolve(parseInt(maxPid))
+        })
+    })
+}
+
 function main() {
     let doNothing = () => null
-    fetchPost(10700).then(pid => fetchRank(pid), doNothing)
-        .then(pid => logger.log('[done]', pid), doNothing)
+
+    let currentIndex = -1
+    let beginPid = 53
+    let queryList
+    let maxConcurrent = 3
+    let delay = 1000
+    let taskDelay = 100
+
+    let _next = () => {
+        if (currentIndex < queryList.length) {
+            let nextPid = queryList[++currentIndex]
+            setTimeout(() => {
+                nextPromise(nextPid)
+            }, delay)
+        }
+    }
+
+    let nextPromise = (pid) => {
+        return fetchPost(pid)
+            .then(pid => fetchRank(pid), (pid) => _next())
+            .then(pid => {
+                if (pid == null) return
+                logger.log('[done]', pid)
+                _next()
+            }, doNothing)
+    }
+
+    getMaxPid().then(mpid => {
+        queryList = _.range(beginPid, mpid)
+        for (let i = 0; i < maxConcurrent; i++) {
+            setTimeout(() => {
+                nextPromise(queryList[++currentIndex])
+            }, taskDelay * i)
+
+        }
+    })
+
 }
 
 main()
