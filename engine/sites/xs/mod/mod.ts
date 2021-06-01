5f85fa4fbd12800917bcbe9cc0cd89d645bd3244
Wing Gao <wing.gao@live.com>
2021-06-02 00:53:55 +0800

    feat: update

--- engine/sites/xs/mod/mod.ts
+++ engine/sites/xs/mod/mod.ts
@@ -1,47 +1,74 @@
-import { Entity, ObjectID, ObjectIdColumn, Column, getManager } from "typeorm";
+import { Entity, ObjectIdColumn, Column, getManager, getMongoRepository, MongoRepository } from "typeorm";
+import { ObjectID } from "mongodb";
 
 @Entity()
 export class Person {
-    @ObjectIdColumn()
-    id: ObjectID;
+  @ObjectIdColumn()
+  id: ObjectID;
 
-    @Column()
-    cnName: string
+  @Column()
+  cnName: string
+
+  toString() {
+    return `Person{id=${this.id},cnName=${this.cnName}}`
+  }
 }
 
 @Entity()
 export class PageResult {
-    @ObjectIdColumn()
-    id: ObjectID;
-    @Column()
-    url: string
-    @Column()
-    html: string
-    @Column()
-    contentPre: string
-
-    @Column()
-    srcType: string
+  @ObjectIdColumn()
+  id: ObjectID;
+  @Column()
+  url: string
+  @Column()
+  html: string
+  @Column()
+  contentPre: string
+  @Column()
+  srcType: string
+  @Column()
+  personId: ObjectID
 }
 
 export const SrcType = {
-    Wiki: 'wiki'
+  Wiki: 'wiki',
+  BaiduWiki: 'baidu-wiki'
 }
 
-import { createConnection, Connection } from "typeorm";
+import { createConnection, Connection } from 'typeorm';
 
 let connection: Connection
+export let pageRepo: MongoRepository<PageResult> & IPageRepoExt
+
+interface IPageRepoExt {
+  findByUserType(personId: ObjectID, srcType: string): Promise<PageResult[]>
+
+  upsertByUrl(p: PageResult): Promise<Boolean>
+}
+
+function extendPageResult() {
+  pageRepo.findByUserType = (personId: ObjectID, srcType: string) => {
+    return pageRepo.find({ personId, srcType })
+  }
+  pageRepo.upsertByUrl = async (pr: PageResult) => {
+    let res = await pageRepo.updateOne({ url: pr.url }, { $set: pr }, { upsert: true })
+    return res.upsertedCount > 0
+  }
+}
+
 
 export async function initDB() {
-    connection = await createConnection({
-        type: "mongodb",
-        host: "localhost",
-        port: 27017,
-        database: "xs",
-        entities: [
-            __dirname + "/*.ts"
-        ]
-    });
-    return getManager();
-
+  connection = await createConnection({
+    type: "mongodb",
+    // host: "localhost",
+    host: "ubu.vm",
+    port: 27017,
+    database: "xs",
+    entities: [
+      __dirname + "/*.ts"
+    ]
+  });
+  pageRepo = getMongoRepository(PageResult) as any
+  extendPageResult()
+  return getManager();
 }
--- engine/sites/xs/web.ts
+++ engine/sites/xs/web.ts
@@ -1,44 +1,76 @@
 import wiki from 'wikijs';
 import { getLogger } from "log4js";
-import { initDB, PageResult, Person, SrcType } from "./mod";
-import { EntityManager, getMongoRepository } from "typeorm";
+import { initDB, pageRepo, PageResult, Person, SrcType } from "./mod";
+import { EntityManager, getMongoRepository } from 'typeorm';
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 const logger = getLogger();
 logger.level = 'debug';
 let manager: EntityManager
 
 
-async function searchWiki(name) {
-    // https://www.npmjs.com/package/wikijs
-    await wiki({ apiUrl: 'https://zh.wikipedia.org/w/api.php' })
-        .page(name)
-        // .then(page => page.info('alterEgo'))
-        .then(async (page) => {
-            //TODO 多页查询
-            let pr = new PageResult()
-            pr.srcType = SrcType.Wiki;
-            pr.url = page.url();
-            pr.contentPre = await page.content()
-            pr.html = await page.html()
-            logger.info(page)
-            let res = await getMongoRepository(PageResult).updateOne({url:pr.url},pr,{upsert:true})
-            logger.info(res)
-        }); // Bruce Wayne
+async function searchWiki(person: Person) {
+  let res = await pageRepo.findByUserType(person.id, SrcType.Wiki)
+  if (res.length > 0) {
+    logger.info(`[${person}][searchWiki] 已有${res.length}`)
+    return
+  }
+  // https://www.npmjs.com/package/wikijs
+  await wiki({ apiUrl: 'https://zh.wikipedia.org/w/api.php' })
+    .page(person.cnName)
+    // .then(page => page.info('alterEgo'))
+    .then(async (page) => {
+      //TODO 多页查询
+      let pr = new PageResult()
+      pr.personId = person.id
+      pr.srcType = SrcType.Wiki;
+      pr.url = page.url();
+      pr.contentPre = JSON.stringify(await page.content())
+      pr.html = await page.html()
+      logger.info(page)
+      let res = await getMongoRepository(PageResult).updateOne({ url: pr.url }, { $set: pr }, { upsert: true })
+      logger.info(res)
+    }); // Bruce Wayne
+}
+
+async function searchBaiduWiki(person: Person) {
+  let res = await pageRepo.findByUserType(person.id, SrcType.BaiduWiki)
+  if (res.length > 0) {
+    logger.info(`[${person}][searchBaiduWiki] 已有${res.length}`)
+    return
+  }
+  let pUrl = `https://baike.baidu.com/item/${encodeURIComponent(person.cnName)}`
+  logger.info('searchBaiduWiki', pUrl)
+  let rep = await axios.get(pUrl)
+  let pr = new PageResult()
+  pr.personId = person.id
+  pr.srcType = SrcType.BaiduWiki;
+  pr.url = pUrl;
+  pr.html = rep.data
+  let $ = cheerio.load(pr.html)
+  pr.contentPre = $('body').text()
+  logger.info('searchBaiduWiki add', pr)
+  await pageRepo.upsertByUrl(pr)
+
 }
 
 async function searchName(name) {
-    let person = await manager.findOne(Person, { cnName: name })
-    if (person == null) {
-        person = new Person()
-        person.cnName = name
-        await manager.save(person)
-    }
-    await searchWiki(name)
+  let person = await manager.findOne(Person, { cnName: name })
+  if (person == null) {
+    person = new Person()
+    person.cnName = name
+    await manager.save(person)
+  }
+  await Promise.all([
+    searchWiki(person), searchBaiduWiki(person)
+  ])
+
 }
 
 async function main() {
-    manager = await initDB()
-    await searchName('饶毅')
+  manager = await initDB()
+  await searchName('饶毅')
 }
 
 main()
