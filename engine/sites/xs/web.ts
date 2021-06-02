d2a532b56ca97f608dca53901d3173672448ea28
WingGao <wing.gao@live.com>
2021-06-02 21:59:21 +0800

    feat: 更新

--- engine/sites/xs/web.ts
+++ engine/sites/xs/web.ts
@@ -2,8 +2,9 @@ import wiki from 'wikijs';
 import { getLogger } from "log4js";
 import { initDB, pageRepo, PageResult, Person, SrcType } from "./mod";
 import { EntityManager, getMongoRepository } from 'typeorm';
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import * as cheerio from "cheerio";
+import _ = require("lodash");
 
 const logger = getLogger();
 logger.level = 'debug';
@@ -11,9 +12,10 @@ let manager: EntityManager
 
 
 async function searchWiki(person: Person) {
+    let tag =`[${person}][searchWiki]`
   let res = await pageRepo.findByUserType(person.id, SrcType.Wiki)
   if (res.length > 0) {
-    logger.info(`[${person}][searchWiki] 已有${res.length}`)
+    logger.info(`${tag} 已有${res.length}`)
     return
   }
   // https://www.npmjs.com/package/wikijs
@@ -21,6 +23,12 @@ async function searchWiki(person: Person) {
     .page(person.cnName)
     // .then(page => page.info('alterEgo'))
     .then(async (page) => {
+        // @ts-ignore
+        if(page.raw.pageprops.disambiguation != null){
+            //设置个歧义页面
+            logger.error(`${tag} 有歧义 ${page.raw.fullurl}`)
+            return
+        }
       //TODO 多页查询
       let pr = new PageResult()
       pr.personId = person.id
@@ -31,28 +39,64 @@ async function searchWiki(person: Person) {
       logger.info(page)
       let res = await getMongoRepository(PageResult).updateOne({ url: pr.url }, { $set: pr }, { upsert: true })
       logger.info(res)
-    }); // Bruce Wayne
+    }).catch(e=>{
+        if(e.message == 'No article found'){
+            logger.error(`${tag} 无法找到`)
+        }else{
+            throw e
+        }
+      }); // Bruce Wayne
 }
 
 async function searchBaiduWiki(person: Person) {
+    let tag =`[${person}][searchBaiduWiki]`
   let res = await pageRepo.findByUserType(person.id, SrcType.BaiduWiki)
   if (res.length > 0) {
-    logger.info(`[${person}][searchBaiduWiki] 已有${res.length}`)
+    logger.info(`${tag} 已有${res.length}`)
     return
   }
   let pUrl = `https://baike.baidu.com/item/${encodeURIComponent(person.cnName)}`
-  logger.info('searchBaiduWiki', pUrl)
-  let rep = await axios.get(pUrl)
-  let pr = new PageResult()
-  pr.personId = person.id
-  pr.srcType = SrcType.BaiduWiki;
-  pr.url = pUrl;
-  pr.html = rep.data
-  let $ = cheerio.load(pr.html)
-  pr.contentPre = $('body').text()
-  logger.info('searchBaiduWiki add', pr)
-  await pageRepo.upsertByUrl(pr)
-
+    return fetchUrl(person,{
+        url: pUrl,
+        onResponse:async(rep,pr,$)=>{
+            let poly = $('.polysemant-list-lemma-title')
+            if(poly.length > 0){
+                logger.error(`${tag} 有歧义 ${pUrl} ${poly.text()} `)
+                return false
+            }
+            pr.srcType = SrcType.BaiduWiki;
+            return true
+        },
+    })
+}
+interface IFetchUrlConfig {
+    url:string,
+    onResponse?: (rep:AxiosResponse<any>,pr:PageResult,$:cheerio.CheerioAPI)=>Promise<boolean>
+    checkExist?: boolean //默认true
+}
+async function fetchUrl(person:Person,config:IFetchUrlConfig){
+    config = _.merge({checkExist:true},config)
+    if(config.checkExist){
+        let res = await pageRepo.find({personId:person.id,url:config.url})
+        if (res.length > 0) {
+            logger.info(`[${person}][fetchUrl] 已有${res.length}`)
+            return
+        }
+    }
+    logger.info('fetchUrl', config.url)
+    let rep = await axios.get(config.url)
+    let pr = new PageResult()
+    pr.personId = person.id
+    pr.url = config.url;
+    pr.html = rep.data
+    let $ = cheerio.load(pr.html)
+    pr.contentPre = $('body').text()
+    let canAdd = true
+    if(config.onResponse) canAdd=await config.onResponse(rep,pr,$ as any)
+    if(canAdd) {
+        logger.info('fetchUrl add', pr)
+        await pageRepo.upsertByUrl(pr)
+    }
 }
 
 async function searchName(name) {
@@ -70,7 +114,9 @@ async function searchName(name) {
 
 async function main() {
   manager = await initDB()
-  await searchName('饶毅')
+  // await searchName('饶毅')
+  // await searchName('王晓东')
+  await searchName('杨朝勇')
 }
 
 main()
